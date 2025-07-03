fa2c1b9416c9e8a68e3f73400db346c937069ae3
alessandro <alessandrotibia664@gmail.com>
2025-07-03 20:51:48 -0300

    Update and rename códigosql.sql to código_trabalho.sql

--- código_trabalho.sql
+++ código_trabalho.sql
@@ -2,7 +2,7 @@ SELECT department, FLOOR(SUM(salary)/9 + (SUM(salary)/9) * 0.15) AS salario_medi
 FLOOR(MAX(salary) + MAX(salary) * 0.15) AS maior_salario,
 FLOOR(MAX(salary) + MAX(salary) * 0.15) - FLOOR(SUM(salary)/9 + (SUM(salary)/9) * 0.15) AS diferença_absoluta
 FROM employees
-WHERE last_name LIKE '%n_%'
+WHERE last_name LIKE '%n_%' AND department IN ("Marketing", "Bakery")
 GROUP BY department
 ORDER BY FLOOR(SUM(salary)/9) DESC
 LIMIT 1
